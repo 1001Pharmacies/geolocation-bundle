c9278a10febbd7554bb7960d791d72dbd954d9bf
methylbro <thomas@gasc.fr>
2014-12-10 12:19:11 +0100

    add LocatorCompilerPass

--- MeupGeoLocationBundle.php
+++ MeupGeoLocationBundle.php
@@ -1,9 +1,31 @@
 <?php
 
-namespace Meup\Bundle\ProductBundle;
+/**
+* This file is part of the Meup GeoLocation Bundle.
+*
+* (c) 1001pharmacies <http://github.com/1001pharmacies/geolocation-bundle>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Meup\Bundle\GeoLocationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Meup\Bundle\GeoLocationBundle\DependencyInjection\DependencyInjection\LocatorCompilerPass;
 
+/**
+ *
+ */
 class MeupGeoLocationBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new LocatorCompilerPass());
+    }
 }
