767b74278bd2cadb3d5df626037e51831b9fe650
methylbro <thomas@gasc.fr>
2015-01-05 13:56:09 +0100

    Unused use statement should be avoided

---
+++ Command/LocateCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+* This file is part of the Meup GeoLocation Bundle.
+*
+* (c) 1001pharmacies <http://github.com/1001pharmacies/geolocation-bundle>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Meup\Bundle\GeoLocationBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ *
+ */
+class LocateCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('meup_geolocation:locator:locate')
+            ->setDescription('Get the weekly movies sessions program.')
+            //->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet?')
+            //->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters')
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $locator = $this
+            ->getContainer()
+            ->get('meup_geolocation.locator')
+        ;
+    }
+}
